a18d1a97bf31b7ee68827533a89a6df5c7b1643f
Alan Alvarenga <Alan.AlvarengaMejia@telus.com>
2020-04-08 22:49:32 -0600

    Adding first component

---
+++ src/components/Home/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from 'antd'
+
+export default function Home(props) {
+  return (
+    <>
+    <Button type="primary">Primary</Button>
+    </>
+  )
+};
