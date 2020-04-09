831507c657212662be7e36090521ebcc9b4cab8b
Alan Alvarenga <Alan.AlvarengaMejia@telus.com>
2020-04-08 23:41:47 -0600

    calling external API

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { Button } from 'antd'
-
+import axios from 'axios';
+const get = () => {
+  axios.get('http://localhost:8888/deploy/new')
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+}
 export default function Home(props) {
   return (
     <>
-    <Button type="primary">Primary</Button>
+    <Button type="primary" onClick={get}>Primary</Button>
     </>
   )
 };
